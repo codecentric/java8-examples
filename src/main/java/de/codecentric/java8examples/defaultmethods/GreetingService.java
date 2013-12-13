1030082b968d593b06ec634851f2d4c67fcb7913
Benedikt Ritter <beneritter@gmail.com>
2013-12-13 13:16:09 +0100

    JavaDoc for default method implementation

--- src/main/java/de/codecentric/java8examples/defaultmethods/GreetingService.java
+++ src/main/java/de/codecentric/java8examples/defaultmethods/GreetingService.java
@@ -5,6 +5,11 @@ package de.codecentric.java8examples.defaultmethods;
  */
 public interface GreetingService {
 
+    /**
+     * Creates a greeting message. The provided default implementation simply returns "Hello world!"
+     *
+     * @return A greeting message.
+     */
     default String greet() {
         return "Hello World!";
     }
